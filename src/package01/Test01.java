83b1696744d006e953f70fc5d955819967dae6ae
张三 <11@qq.com>
2020-04-18 12:24:13 +0800

    11111

---
+++ src/package01/Test01.java
@@ -0,0 +1,12 @@
+package package01;
+
+/**
+ * @author 刘鹏
+ * @Description
+ * @create 2020-04-18 12:20
+ */
+public class Test01 {
+    public static void main(String[] args) {
+        System.out.println("2020年4月18日12:20:53");
+    }
+}
